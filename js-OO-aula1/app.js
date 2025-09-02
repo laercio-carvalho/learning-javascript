55600b108aefc940cea19be2ca2f8c0d903e2147
laercio-carvalho <laercio.carvalho.dev@gmail.com>
2025-09-01 23:22:03 -0300

    LEARNING - Termino da aula 01 de orintação a objeto com a utilização de funções com uso do console, prompt, alert, input e soma de dados

---
+++ js-OO-aula1/app.js
@@ -0,0 +1,8 @@
+let titulo = document.querySelector("h1");
+titulo.innerHTML = 'Hora do Desafio';
+let paragrafo = document.querySelector("p");
+paragrafo.innerHTML = 'Escolha um numero entre 1 e 10';
+
+function verificarChute(){
+    console.log('O Botão foi clicado');
+}
